0a3dd960fa1774196a3855cf27efb466f42eb88b
Amber <aford2008@gmail.com>
2015-09-10 12:06:03 -0400

    Zac and Amber added submitForm function and added input values into object

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-
-//Confirms the user's reservation with a pop-up.
-function confirmation() {
-    confirm("Please select OK to finalize your reservation.");
-}
+var attendees = [];
+var numOfSeats = 16;
 
 $(document).ready(function(){
 	//Displays the user's name after completethey complete the form. 
@@ -12,13 +9,37 @@ $(document).ready(function(){
 
     var $form = $('form');
 
-//Displays the form once a seat has been selected. 
+	//Displays the form once a seat has been selected. 
     function showForm()  {
-    $('.seat').addClass('inactive');
-    $(this).removeClass('inactive');
-    $form.fadeIn();
+	    $('.seat').addClass('inactive');
+	    $(this).removeClass('inactive');
+	    $form.fadeIn();
     }
-//event handler
-$('.seat').click(showForm);
+
+    function submitForm(e) {
+    	e.preventDefault();
+
+    	var name = $('#name').val();
+    	var email = $('#email').val();
+
+    	user = {};
+    	user.name = name;
+    	user.email = email;
+
+    	attendees.push(user);
+
+    	console.log(attendees);
+
+    	$form.trigger('reset');
+    }
+
+	//event handler
+	$('.seat').click(showForm);
+	$('#submit').click(submitForm);
+
 });
 
+//Confirms the user's reservation with a pop-up.
+function confirmation() {
+    confirm("Please select OK to finalize your reservation.");
+}
