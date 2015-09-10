89b977ba73b42a638c367115f9e59b692c0b397b
Amber <aford2008@gmail.com>
2015-09-10 10:53:36 -0400

    corrected input type for name

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function confirmation() {
     confirm("Please select OK to finalize your reservation.");
 }
+
+$(document).ready(function(){
+    $("button").click(function(){
+        alert("You have reserved a seat for " + $("#name").val()) + ".";
+    });
+});
+
