528f40b2c5cb190090b4e14fc7c2820ee62415b1
Amber <aford2008@gmail.com>
2015-09-10 11:44:12 -0400

    Zac added showForm function on seat click

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+
+//Confirms the user's reservation with a pop-up.
 function confirmation() {
     confirm("Please select OK to finalize your reservation.");
 }
 
 $(document).ready(function(){
+	//Displays the user's name after completethey complete the form. 
     $("button").click(function(){
         alert("You have reserved a seat for " + $("#name").val()) + ".";
     });
+
+    var $form = $('form');
+
+//Displays the form once a seat has been selected. 
+    function showForm()  {
+    $('.seat').addClass('inactive');
+    $(this).removeClass('inactive');
+    $form.fadeIn();
+    }
+//event handler
+$('.seat').click(showForm);
 });
 
